0d54fb1fa6c992fe7a34da4fbe9079ab0427b01f
abdlynng <abdlynng@gmail.com>
2020-04-07 19:01:43 +0200

    jeu de pendu 1.0 : on a utilisé un mot qui a été declaré avant

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
 ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆*/
 #include <stdio.h>
 #include <stdlib.h>
+#include "jeu.h"
 
 int main()
 {
     printf("\t============================================\n"
             "\t**********\tJEU DE PENDU\t *********** \n"
            "\t============================================\n\n\t");
+
+    char *motSecret ="BONJOUR";
+    char *motGrille =  malloc(strlen(motSecret) * sizeof(char) );
+    jouer(motSecret, motGrille);
 
     return 0;
 }
